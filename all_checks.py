703bc95c332d8e50f4bc51f6e9a77b20c215ac01
TahaHusnain <taha@LAPTOP-MUO14OKV>
2024-04-12 18:04:42 +0500

    Allow printing more than one error message

--- all_checks.py
+++ all_checks.py
@@ -1,6 +1,8 @@
-atom all_checks.py
-#!/usr/binenv python3
-(...)
+#!/usr/bin/env python3
+
+import shutil
+import sys
+
 def check_disk_full(disk, min_gb, min_percent):
     """Returns True if there isn't enough disk space, False otherwise."""
     du = shutil.disk_usage(disk)
@@ -18,16 +20,20 @@ def check_root_full():
 
 def main():
     checks = [
-            (check_reboot, "Pending Reboot."),
             (check_root_full, "Root partition full"),
             ]
 
+    everything_ok = True
     for check, msg in checks:
         if check():
             print(msg)
-            sys.exit(1)
+            everything_ok = False
+
+    if not everything_ok:
+        sys.exit(1)
 
     print("Everything ok.")
     sys.exit(0)
 
-main()
+if __name__ == "__main__":
+    main()
